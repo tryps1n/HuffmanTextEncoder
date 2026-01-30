1d5cb9a537e865b102154b9206df5518e3ad192b
tryps1n <rangan5646@gmail.com>
2026-01-31 03:39:41 +0800

    Bkikljklj D C C

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,88 @@
+#include <filesystem>
 #include <iostream>
 #include <string>
-#include <unordered_map>
-#include "lib.h"
+#include "fstream"
+#include <vector>
+#include "hufflib.cpp"
+#include "iolib.cpp"
 
 using namespace std;
 
+int main(int argc, char* argv[])
+{
+    if (argc < 2)
+    {
+        cout << "Usage: " << argv[0] << " <flag> [files...]";
+        cout << "Flags:" << endl;
+        cout << "  -e <input> <output>  Encode/compress" << endl;
+        cout << "  -d <input> <output>  Decode/decompress" << endl;
+        return 1;
+    }
 
-int main(){
+    vector<string> arg;
+    for(int i=1; i<argc; ++i)
+    {
+        arg.push_back((string)argv[i]); 
+    }
 
-    string s;
-    getline(cin, s);
-
-    unordered_map<char, int> freq = count_freq_string(s);
-    vector<Node*> nodes = nodes_from_freq(freq);
-    Node* root = build_Huffman_Tree(nodes);
-
-    unordered_map<char, string> hufftable;
-    build_Huffman_table(root, "0", hufftable);
-
-    string encoded = Huffman_Encode(s);
+    string path_to_in_file = filesystem::current_path().string() + "/" + arg[1];
     
-    cout << encoded << endl;
+    if (!filesystem::exists(path_to_in_file)) 
+    {
+        cout << "invalid file name" << endl;
+        return 1;
+    }
 
-    cout << "decoded: " << Huffman_Decode(encoded, hufftable);
+    if (arg[0] == "-e")
+    {
+        ifstream infile(path_to_in_file);
+        if (!infile.is_open())
+        {
+            cerr << "Error opening file" << endl;
+            return 1;
+        }
+        string line, plain;
+        while (getline(infile, line))
+        {
+            plain += line;
+        }
 
+        unordered_map<char, int> huff_freq = count_freq_string(plain);
+        vector<Node*> nodes = nodes_from_freq(huff_freq);
+        Node* root = build_Huffman_Tree(nodes);
+
+        unordered_map<char, string> hufftable;
+        build_Huffman_table(root, "0", hufftable);
+
+        string encoded = Huffman_Encode(plain);
+        
+        fstream outfile(arg[1]+".bin", ios::binary | ios::out);
+
+        write_bin_to_file(outfile, encoded);
+
+        outfile.close();
+
+        cout << "written successfully" << endl;
+        
+        // debug
+
+        fstream binfile(arg[1]+".bin", ios::binary | ios::in);
+        if (!binfile.is_open())
+        {
+            cerr << "Error opening file";
+            return 1;
+        }
+        
+        string decoded, bindata;
+    }
+    else if (arg[0] == "-d") {
+        // smth
+    }  
+    else
+    {   
+        cout << "Wrong flag. Usage: " << endl;
+        cout << "\"-e\" - encode" << endl;
+        cout << "\"-d\" - decode" << endl; 
+    }
     return 0;
 }
